bd493b8e0db5904ecc45401de1824feac122079c
CharlesMwangi <bsclmr696619@spu.ac.ke>
2023-07-25 11:31:04 +0300

    Small twists

--- index.tsx
+++ index.tsx
@@ -1,15 +1,13 @@
+document.getElementById("a-score").innerText = "0";
+document.getElementById("b-score").textContent = 0;
+
 let scoreA = document.getElementById("a-score");
 let scoreB = document.getElementById("b-score");
 
-let scoreOfA = 0;
-let scoreOfB = 0;
-
 function incrementA() {
-  scoreOfA += 1;
-  scoreA.textContent = scoreOfA;
+  scoreA.textContent += 1;
 }
 
 function incrementB() {
-  scoreOfB += 1;
-  scoreB.textContent = scoreOfB;
+  scoreB.textContent += 1;
 }
